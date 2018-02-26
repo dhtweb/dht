1e246d057600c51d0c8176557a5194fd988a42fa
zhengkai <molunshang@outlook.com>
2018-02-27 00:14:31 +0800

    记录索引增量更新时间

--- DhtCrawler.Web/Controllers/IndexController.cs
+++ DhtCrawler.Web/Controllers/IndexController.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using DhtCrawler.Service.Index;
+using DhtCrawler.Service.Model;
+using DhtCrawler.Service.Repository;
 using log4net;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,38 +13,56 @@ namespace DhtCrawler.Web.Controllers
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(IndexController));
         private static readonly object IndexLocker = new object();
-        private IndexSearchService _indexSearchService;
-        public IndexController(IndexSearchService indexSearchService)
+        private readonly IndexSearchService _indexSearchService;
+        private readonly StatisticsInfoRepository _statisticsInfoRepository;
+        private readonly IServiceProvider _serviceProvider;
+
+        public IndexController(IndexSearchService indexSearchService, StatisticsInfoRepository statisticsInfoRepository, IServiceProvider serviceProvider)
         {
             _indexSearchService = indexSearchService;
+            _statisticsInfoRepository = statisticsInfoRepository;
+            _serviceProvider = serviceProvider;
         }
 
-        public IActionResult IncrementBuild(DateTime updateTime)
+        public async Task<IActionResult> IncrementBuild()
         {
-            Task.Factory.StartNew(() =>
+            await _statisticsInfoRepository.GetInfoById("LastIndexTime").ContinueWith(t =>
             {
-                try
+                var lastIndexTime = DateTime.Now.AddHours(-1);
+                DateTime? updateTime = null;
+                if (t.Result != null)
                 {
-                    if (!Monitor.TryEnter(IndexLocker))
+                    updateTime = t.Result.UpdateTime;
+                }
+                Task.Factory.StartNew(() =>
+                {
+                    try
                     {
-                        return;
+                        if (!Monitor.TryEnter(IndexLocker))
+                        {
+                            return;
+                        }
+                        log.Info("开始构建索引");
+                        _indexSearchService.IncrementBuild(updateTime);
+                        log.Info("构建索引完成");
+                        using (var repository = (StatisticsInfoRepository)_serviceProvider.GetService(typeof(StatisticsInfoRepository)))
+                        {
+                            repository.InsertOrUpdate(new StatisticsInfoModel() { UpdateTime = lastIndexTime, DataKey = "LastIndexTime" });
+                        }
                     }
-                    log.Info("开始构建索引");
-                    _indexSearchService.IncrementBuild(updateTime);
-                    log.Info("构建索引完成");
-                }
-                catch (Exception ex)
-                {
-                    log.Error("索引构建出错", ex);
-                }
-                finally
-                {
-                    if (Monitor.IsEntered(IndexLocker))
+                    catch (Exception ex)
                     {
-                        Monitor.Exit(IndexLocker);
+                        log.Error("索引构建出错", ex);
                     }
-                }
-            }, TaskCreationOptions.LongRunning);
+                    finally
+                    {
+                        if (Monitor.IsEntered(IndexLocker))
+                        {
+                            Monitor.Exit(IndexLocker);
+                        }
+                    }
+                }, TaskCreationOptions.LongRunning);
+            });
             return new EmptyResult();
         }
 
