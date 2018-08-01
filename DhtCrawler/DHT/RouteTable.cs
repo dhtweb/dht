2f63f357059a27f7dd763561d74a397e903b6010
zhengkai <shengwutai@163.com>
2018-08-02 00:29:53 +0800

    修改节点表

--- DhtCrawler/DHT/RouteTable.cs
+++ DhtCrawler/DHT/RouteTable.cs
@@ -65,7 +65,7 @@ namespace DhtCrawler.DHT
 
         public void AddNode(DhtNode node)
         {
-            if (node.NodeId == null || _kTable.Count >= _maxNodeSize)
+            if (node.NodeId == null || node.NodeId.Length != 20 || _kTable.Count >= _maxNodeSize)
                 return;
             var route = new Route()
             {
@@ -85,7 +85,7 @@ namespace DhtCrawler.DHT
 
         public void AddOrUpdateNode(DhtNode node)
         {
-            if (node.NodeId == null)
+            if (node.NodeId == null || node.NodeId.Length != 20)
                 return;
             if (_kTable.Count >= _maxNodeSize && _minLastTime + RouteLife.Ticks < DateTime.Now.Ticks)
             {
